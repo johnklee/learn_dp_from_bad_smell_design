434b7e929c9f5a7982f4ab26349d528947260887
johnklee <puremonkey2001@yahoo.com.tw>
2022-03-02 21:55:11 +0800

    add solution of factory part1

---
+++ smart-pizza-store_factory-part1_solution/pizza_factory.py
@@ -0,0 +1,24 @@
+import pizza_recipes
+
+
+class Error(Exception):
+  pass
+
+
+class UnknownPizzaNameError(Error):
+  def __init__(self, name):
+    super().__init__(f'Pizza name={name} is unknown!')
+
+
+class SimpleFactory:
+  def create_pizza(self, pizza_name: str) -> pizza_recipes.Pizza:
+    if pizza_name == 'cheeze':
+      return pizza_recipes.CheezePizza()
+    elif pizza_name == 'greek':
+      return pizza_recipes.GreekPizza()
+    elif pizza_name == 'pepper':
+      return pizza_recipes.PepperoniPizza()
+    elif pizza_name == 'veg':
+      return pizza_recipes.VeggiePizza()
+    else:
+      raise UnknownPizzaNameError(pizza_name)
