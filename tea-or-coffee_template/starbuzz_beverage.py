762a8fe3b5282424ee03228812a195aecca6407b
johnklee <puremonkey2001@yahoo.com>
2022-05-08 14:03:35 +0800

    enhance tea-of-coffee package

---
+++ tea-or-coffee_template/starbuzz_beverage.py
@@ -0,0 +1,43 @@
+"""StarBuzz Beverage recipes."""
+import abc
+
+
+_STEP_BOIL_WATER = 'Boil some water'
+_STEP_PULL_DRINK = 'Pour drink in cup'
+_STEP_GRIND_COFFEE_BEAN = 'Grind coffee bean'
+
+
+class CaffeineBeverage(abc.ABC):
+  """Receipe of Caffeine Beverage."""
+
+  @abc.abstractmethod
+  def make_drink(self, action_list: List[str]):
+    raise NotImplementedError
+
+  @abc.abstractmethod
+  def add_condiment(self, action_list: List[str]):
+    raise NotImplementedError
+
+  def boil_water(self, action_list: List[str]):
+    action_list.append(_STEP_BOIL_WATER)
+
+  def pull_drink(self, action_list: List[str]):
+    action_list.append(_STEP_PULL_DRINK)
+
+  def is_coffee(self):
+    return False
+
+  def grind_coffee_bean(self, action_list: List[str]):
+    action_list.append(_STEP_GRIND_COFFEE_BEAN)
+
+  def make(self) -> List[str]:
+    action_list = []
+    if self.is_coffee():
+      self.grind_coffee_bean()
+
+    self.boil_water(action_list)
+    self.make_drink(action_list)
+    self.pull_drink(action_list)
+    self.add_condiment(action_list)
+
+    return action_list
--- tea-or-coffee_template/starbuzz_coffee.py
+++ tea-or-coffee_template/starbuzz_coffee.py
@@ -2,15 +2,18 @@
 
 from typing import List
 
+
 _STEP_BOIL_WATER = 'Boil some water'
+_STEP_GRIND_COFFEE_BEAN = 'Grind coffee bean'
 _STEP_MAKE_DRINK = 'Brew coffee in boiling water'
-_STEP_PULL_DRINK = 'Pour coffee in cup'
+_STEP_PULL_DRINK = 'Pour drink in cup'
 _STEP_ADD_CONDIMENT = 'Add sugar and milk'
 
 
 class StarBuzzCoffee:
   def make(self) -> List[str]:
     action_list = []
+    action_list.append(_STEP_GRIND_COFFEE_BEAN)
     action_list.append(_STEP_BOIL_WATER)
     action_list.append(_STEP_MAKE_DRINK)
     action_list.append(_STEP_PULL_DRINK)
