73a5f03239f1a2a2eaa99579454b2b320e4b7c27
selinahuang <selinahuang@google.com>
2022-05-16 22:49:34 +0800

    my solution

--- tea-or-coffee_template/starbuzz_coffee.py
+++ tea-or-coffee_template/starbuzz_coffee.py
@@ -1,22 +1,19 @@
 """StarBuzz Coffee recipe."""
 
+import starbuzz_beverage
 from typing import List
 
 
-_STEP_BOIL_WATER = 'Boil some water'
-_STEP_GRIND_COFFEE_BEAN = 'Grind coffee bean'
 _STEP_MAKE_DRINK = 'Brew coffee in boiling water'
-_STEP_PULL_DRINK = 'Pour drink in cup'
 _STEP_ADD_CONDIMENT = 'Add sugar and milk'
 
 
-class StarBuzzCoffee:
-  def make(self) -> List[str]:
-    action_list = []
-    action_list.append(_STEP_GRIND_COFFEE_BEAN)
-    action_list.append(_STEP_BOIL_WATER)
+class StarBuzzCoffee(starbuzz_beverage.CaffeineBeverage):
+  def make_drink(self, action_list: List[str]):
     action_list.append(_STEP_MAKE_DRINK)
-    action_list.append(_STEP_PULL_DRINK)
-    action_list.append(_STEP_ADD_CONDIMENT)
 
-    return action_list
+  def add_condiment(self, action_list: List[str]):
+    action_list.append(_STEP_ADD_CONDIMENT)
+  
+  def is_coffee(self):
+    return True
--- tea-or-coffee_template/starbuzz_tea.py
+++ tea-or-coffee_template/starbuzz_tea.py
@@ -1,19 +1,15 @@
 """StarBuzz Coffee recipe."""
 
+import starbuzz_beverage
 from typing import List
 
-_STEP_BOIL_WATER = 'Boil some water'
 _STEP_MAKE_DRINK = 'Steep tea in boiling water'
-_STEP_PULL_DRINK = 'Pour drink in cup'
 _STEP_ADD_CONDIMENT = 'Add lemon'
 
 
-class StarBuzzTea:
-  def make(self) -> List[str]:
-    action_list = []
-    action_list.append(_STEP_BOIL_WATER)
+class StarBuzzTea(starbuzz_beverage.CaffeineBeverage):
+  def make_drink(self, action_list: List[str]):
     action_list.append(_STEP_MAKE_DRINK)
-    action_list.append(_STEP_PULL_DRINK)
-    action_list.append(_STEP_ADD_CONDIMENT)
 
-    return action_list
+  def add_condiment(self, action_list: List[str]):
+    action_list.append(_STEP_ADD_CONDIMENT)
